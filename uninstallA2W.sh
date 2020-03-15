58b46d025e4563e69cc58ba5c47e2370a72e5b37
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2020-03-16 00:19:04 +0100

    Update uninstallA2W.sh

--- uninstallA2W.sh
+++ uninstallA2W.sh
@@ -1,7 +1,6 @@
 #/bin/sh
-cd
-rm .config/autostart/xmodmap.desktop
-rm .Xmodmap-A2W
-if [ -f .config/openbox/lxde-rc-org.xml ]; then
-    mv .config/openbox/lxde-rc-org.xml .config/openbox/lxde-rc.xml
+rm ~/.config/autostart/xmodmap.desktop
+rm ~/.Xmodmap-A2W
+if [ -f ~/.config/openbox/lxde-rc-org.xml ]; then
+    mv ~/.config/openbox/lxde-rc-org.xml ~/.config/openbox/lxde-rc.xml
 fi
